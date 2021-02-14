d9e81322f65a7c4310f9d6bbb263dd43c860db2a
Alba G <3.1415@gmail.com>
2021-02-13 21:42:20 -0500

    added postgres

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const cool = require("cool-ascii-faces");
 const express = require("express");
 const path = require("path");
@@ -8,5 +16,17 @@ express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
+  .get("/db", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM test_table");
+      const results = { results: result ? result.rows : null };
+      res.render("pages/db", results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .get("/cool", (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
