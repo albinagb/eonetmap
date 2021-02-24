f8dc8f9022286274fa91fb3769a778566222d256
Alba G <3.1415@gmail.com>
2021-02-23 21:08:07 -0500

    added connection to db

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -8,6 +16,18 @@ app
   .use(express.static(publicPath))
   .get("*", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
+  })
+  .get("/db", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM test_table");
+      const results = { results: result ? result.rows : null };
+      res.render("pages/db", results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
   })
   .listen(PORT, () => {
     console.log(`Server is up on ${PORT}!`);
