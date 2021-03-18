a514ec94b1e198ce99c472acede599c9391c3dbf
Alba G <3.1415ag@gmail.com>
2021-03-18 13:35:53 -0400

    deleted hero

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import data from "./data";
 import MainMap from "./MainMap";
 import "leaflet/dist/leaflet.css";
 import "./styles.css";
-import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
-      
       <MainMap data={data} />
     </>
   );
